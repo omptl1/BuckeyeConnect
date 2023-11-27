ba7e35beb4049c82f99d58ab27295e7caac16eb9
pengyu <chen.10921@osu.edu>
2023-11-27 09:10:22 -0500

    fix the validation

--- finalProject/app/models/user.rb
+++ finalProject/app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
     has_secure_password
     enum role: { student: 0, teacher: 1, team_bob_member: 2 }
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :role, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true, length: { minimum: 8 }
+    validates :password_confirmation, presence: true
 end
