c28ba2c694cff0e6bf9415759b548f31e2a04594
halter151 <halter.151@buckeyemail.osu.edu>
2023-11-30 03:15:51 -0500

    updated home page and some minor html changes

--- finalProject/app/controllers/application_controller.rb
+++ finalProject/app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
 class ApplicationController < ActionController::Base
-    def hello
-            render html: "Hello, World!"
-    end
-    Rails.application.routes.draw do
-        root "application#hello"
-    end
+
 end
--- finalProject/config/routes.rb
+++ finalProject/config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
-  get 'static_pages/help'
   resources :evaluations
   resources :presentations
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root 'evaluations#index'
+  root 'static_pages#home'
 end
