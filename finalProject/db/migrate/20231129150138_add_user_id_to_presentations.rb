db69b3129d182d097c8eb8575ccb482f62e1e90a
halter151 <halter.151@buckeyemail.osu.edu>
2023-12-04 09:06:24 -0500

    fixed devise user error

--- finalProject/db/migrate/20231129150138_add_user_id_to_presentations.rb
+++ finalProject/db/migrate/20231129150138_add_user_id_to_presentations.rb
@@ -1,5 +1,5 @@
 class AddUserIdToPresentations < ActiveRecord::Migration[7.0]
   def change
-    add_reference :presentations, :user, null: false, foreign_key: true
+    #add_reference :presentations, :user, null: false, foreign_key: true
   end
 end
--- finalProject/db/schema.rb
+++ finalProject/db/schema.rb
@@ -26,9 +26,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_03_042914) do
     t.string "title"
     t.text "description"
     t.datetime "date"
+    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_presentations_on_user_id"
   end
 
@@ -40,6 +40,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_03_042914) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "evaluations", "presentations"
