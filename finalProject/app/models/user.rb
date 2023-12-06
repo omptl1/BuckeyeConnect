e0e94b440ce9acae4fb074e4b264da4ac1ad38a8
pengyu <chen.10921@osu.edu>
2023-12-06 14:48:55 -0500

    remove debug

--- finalProject/app/models/user.rb
+++ finalProject/app/models/user.rb
@@ -3,10 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
        :recoverable, :rememberable, :validatable
-  
+
   has_many :presentations, dependent: :destroy
   has_many :evaluations, dependent: :destroy
   has_many :evaluations, through: :presentations
   has_many :evaluations, through: :users
-
+  
 end
