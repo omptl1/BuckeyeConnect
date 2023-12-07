def80a69292cb3aa01e4dee82656d676f7bc4006
halter151 <halter.151@buckeyemail.osu.edu>
2023-12-07 02:05:36 -0500

    users can no longer enter < 0 or > 10 for evaluation.

--- finalProject/app/models/evaluation.rb
+++ finalProject/app/models/evaluation.rb
@@ -2,8 +2,7 @@ class Evaluation < ApplicationRecord
   belongs_to :presentation
   belongs_to :user
   validates :presentation_id, presence: true
-  validates :user_id, presence: true
   validates :score, presence: true
-  validates :score, numericality: { only_integer: true }
+  validates :score, numericality: { less_than: 11, greater_than: 0, only_integer: true }
   validates :comments, presence: true
 end
